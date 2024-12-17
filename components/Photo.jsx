0708ced61b6fab1d5ac8e313b667c4db6604aefc
fangki dev <fangkiigo13@gmail.com>
2024-12-17 11:25:02 +0700

    change photo header

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -20,7 +20,7 @@ const Photo = () => {
                     transition: { delay: 1.4, duration: 0.4, ease: "easeInOut" }
                 }}
             >
-                <div className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten">
+                <div className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px]">
                     <Image
                         src="/assets/photo.png"
                         priority
